23c7ae0bc4ae4e3143528e587d346016d559724a
Vitaliy <dastinkein.vk@gmail.com>
2019-06-06 16:43:22 +0300

    Сюда

--- Так так/script.js
+++ Так так/script.js
@@ -1,12 +1,39 @@
-var howBudget = prompt("Ваш бюджет?", 1), nameMagaze = prompt("Название вашего магазина?", 'Холя моля');
-mainList = {
- budget: howBudget,
- name: nameMagaze,
- shopGoods: [],
+let howBudget = prompt("Ваш бюджет?", 1);
+let nameMagaze = prompt("Название вашего магазина?", 'Холя моля');
+
+let time = 19;
+
+let mainList = {
+    budget: howBudget,
+    name: nameMagaze,
+    shopGoods: [],
+    employers: {},
+    open: false
 }
-var a, b, c;
-a = prompt('Какой тип товаров будем продавать?', ' ');
-b = prompt('Какой тип товаров будем продавать?', ' ');
-c = prompt('Какой тип товаров будем продавать?', ' ');
-mainList.shopGoods.unshift(a, b, c);
-alert ('Ваш бюджет на день: ' + mainList.budget/30);
+
+for (let i = 0; i < 5; i++) {
+
+  let a = prompt("Какой тип товаров будем продавать?");
+
+  if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50) {
+      console.log('Все верно');
+      mainList.shopGoods[i] = a;
+      }
+      else {
+      }
+  }
+
+if (time < 0) {
+  colsole.log('Такого не может быть');
+}
+  else if (time > 8 && time < 20) {
+    console.log('Время работать');
+}
+    else if ( time < 24) {
+      console.log('Уже поздно');
+    }
+     else {
+       console.log ('В сутка 24 часа, сука!');
+     }
+
+alert(mainList.budget / 30);
