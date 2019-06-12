9b49c80aee45aa23ad964d0af0c74d6b4f998d8f
Vitaliy <dastinkein.vk@gmail.com>
2019-06-12 19:10:18 +0300

    сосать

--- Так так/script.js
+++ Так так/script.js
@@ -12,67 +12,68 @@ function start() {
   name = prompt("Название вашего магазина");
   time = 21;
 }
-start();
 
-  var mainList = {
+  let mainList = {
     budget: money,
     shopName: name,
     shopGoods: [],
     employers:{},
     open: false,
-    discount: true
-}
-function chooseGoods(){
-for (var i = 0; i < 5; i++) {
-  var a = prompt("Какой тип товаров будем продавать?");
-  if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
-      console.log('Все верно');
-      mainList.shopGoods[i] = a;
-  }   else {
-        i = i - 1;
-      }
+    discount: false,
+    shopItems: [],
+    chooseGoods: function chooseGoods() {
+        for (var i = 0; i < 5; i++) {
+          let a = prompt("Какой тип товаров будем продавать?");
+          if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+              console.log('Все верно');
+              mainList.shopGoods[i] = a;
+          }   else {
+                i = i - 1;
+              }
+          }
+    },
+      workTime: function workTime(time) {
+        if (time < 0) {
+          colsole.log('Такого не может быть');
+        }
+          else if (time > 8 && time < 20) {
+            console.log('Время работать');
+            mainList.open = true;
+        }
+            else if ( time < 24) {
+              console.log('Уже поздно');
+            }
+             else {
+               console.log ('В сутка 24 часа, сука!');
+             }
+        },
+    dayBudget: function dayBudget() {
+    alert("Ежедневный бюджет" + mainList.budget / 30);
+  },
+  makeDiscount: function makeDiscount() {
+  if (mainList.discount == true) {
+    price = (price/100)*80;
   }
-}
-chooseGoods();
-
-function workTime(time) {
-if (time < 0) {
-  colsole.log('Такого не может быть');
-}
-  else if (time > 8 && time < 20) {
-    console.log('Время работать');
-}
-    else if ( time < 24) {
-      console.log('Уже поздно');
+},
+  hireEmployers: function hireEmployers() {
+    for (i = 1; i < 4; i++) {
+      let name = prompt("Имя сотрудника", "");
+      mainList.employers[i] = name;
     }
-     else {
-       console.log ('В сутка 24 часа, сука!');
-     }
-}
+  },
+  chooseShopItems: function chooseShopItem() {
+    let items = prompt('Перечислите через запятую ваши товары', '');
 
-workTime(18);
-function dailyBudget() {
-alert("Ежедневный бюджет" + mainList.budget / 30);
-}
-
-price = 18;
-function dicountSystem() {
-  if (mainList.discount = true) {
-  alert(price * 0.8);
+    mainList.shopItems = items.split(',');
+    mainList.shopItems.push(prompt('Подождите еще ', ''));
+    mainList.shopItems.sort();
   }
 }
-dicountSystem();
+mainList.chooseShopItems();
+let i = 1;
+mainList.shopItems.forEach(function(item, i, shopItems) {
+  console.log(i + ' : ' + item + ' массив: ' + mainList.shopItems + ")");
+});
 
-function takeEmployers() {
-  for (var b = 1; b < 6; b++) {
-  var v = prompt("Имя?");
-  if ((typeof(v)) === 'string' && (typeof(v)) != null && v != '' && v.length < 50) {
-      console.log('Все верно');
-      mainList.employers[b] = v;
-  }   else {
-        b = b - 1;
-      }
-  }
-}
-takeEmployers();
-console.log(mainList.employers)
+
+console.log(mainList);
